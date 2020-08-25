34a3b9454160b5b8e9b10d1e953d0771c87862b2
David Layec <davidlayec@orange.fr>
2020-08-25 16:46:43 +0200

    nothing yet

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,13 @@ class Event < ApplicationRecord
   has_many :queue_estimations
   has_one :chatroom
   has_one_attached :photo
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
+  validates :line_up, presence: true
+  validates :location, presence: true
 end
