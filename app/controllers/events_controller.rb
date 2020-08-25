9f7882724f8212f3e7d8d091f6435bfc6ede035f
David Layec <davidlayec@orange.fr>
2020-08-25 13:43:24 +0200

    event cont

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    set_sevent
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    set_sevent
+      if @event.destroy
+        flash[:notice] = "\"#{@event.title}\" was successfully deleted."
+        redirect_to events_path
+      else
+        flash.now[:alert] = "There was an error deleting the event."
+        render :show
+      end
+  end
+
+  private
+
+  def set_sevent
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :category, :start_time, :end_time, :address, :price, :line_up)
+  end
+end
