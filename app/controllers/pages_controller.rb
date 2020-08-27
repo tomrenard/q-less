e844f254388b228243e95ec126e7dbcde4ca1aae
Majid Fakhoury <majidfakhoury@gmail.com>
2020-08-27 18:52:17 +0300

    fixed some styling. made navbars cleaner, fixed scrolling, fixed padding in cards for text

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @events = Event.last(3)
+    @events = Event.last(4)
   end
 
   def dashboard
