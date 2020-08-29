96b113c2e8fc48a573ad5a6b451a3126f6b94629
David Layec <davidlayec@orange.fr>
2020-08-29 14:05:31 +0200

    implementation following system, only show to do

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,7 @@
 class Follow < ApplicationRecord
-  belongs_to :follower, foreign_key: :follower_id, class_name: "User"
-  belongs_to :followed_user, foreign_key: :followed_user_id, class_name: "User"
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed_user, class_name: "User"
+
+  validates :follower_id, presence: true
+  validates :followed_user_id, presence: true
 end
