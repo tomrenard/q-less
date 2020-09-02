2dabee2786d983a8e6e47582b7443937b13f5986
David Layec <davidlayec@orange.fr>
2020-09-02 09:58:55 +0200

    css moodif, no search bar

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -32,8 +32,6 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-                                      mapboxgl: mapboxgl }));
     map.addControl(
       new mapboxgl.GeolocateControl({
         positionOptions: {
