caf87da67cc5124511c41927f616878478b99b5f
David Layec <davidlayec@orange.fr>
2020-09-03 13:17:08 +0200

    heart index and show ok

--- app/javascript/components/update.js
+++ app/javascript/components/update.js
@@ -1,17 +1,19 @@
 const initToggleFavorite = () => {
-  const favLink = document.querySelector("#favorite_link")
-  const selectHeart = document.querySelector("#heart")
-  if (favLink) {
-    selectHeart.addEventListener('click', (event) => {
-      if (favLink.dataset.favorite == 'true') {
-        favLink.click()
-        selectHeart.innerHTML = '<i class="fas fa-heart fa-lg"></i>'
-        favLink.dataset.favorite = 'false'
-      } else {
-        favLink.click()
-        selectHeart.innerHTML = '<i class="fas fa-heart-broken fa-lg"></i>'
-        favLink.dataset.favorite = 'true'
-      }
+  const favLinks = document.querySelectorAll("#favorite_link a")
+  const selectHearts = document.querySelectorAll("#heart")
+  if (favLinks[0]) {
+    selectHearts.forEach((selectHeart, index) => {
+      selectHeart.addEventListener('click', (event) => {
+        if (favLinks[index].dataset.favorite == 'true') {
+          favLinks[index].click()
+          selectHeart.innerHTML = '<i class="fas fa-heart-broken fa-lg"></i>'
+          favLinks[index].dataset.favorite = 'false'
+        } else {
+          favLinks[index].click()
+          selectHeart.innerHTML = '<i class="fas fa-heart fa-lg"></i>'
+          favLinks[index].dataset.favorite = 'true'
+        }
+      })
     })
   }
 }
