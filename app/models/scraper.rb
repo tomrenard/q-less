7144165d9f8f3e60e77e85df76a0c6218dbfbedd
Tom Renard <tom.renard@zoho.com>
2021-07-21 00:57:21 +0200

    scraper fixed

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
     urls = []
     locs.each do |loc|
       date = Date.today
-      dates = [date, date + 7]
+      dates = [date]
       dates.each do |d|
         url = "https://ra.co#{loc}?week=#{d}"
         urls << url
@@ -42,10 +42,10 @@ class Scraper
       browser.goto(url)
       html = (open(browser.url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'opera'))
       doc = Nokogiri::HTML(html)
-      links = doc.css('.Box-omzyfs-0.bFNVvf').search('a')
+      links = doc.css('.Box-omzyfs-0.gExFDv').search('a')
       links.each do |link|
         url = link.attribute('href').value
-        events_urls << url if url.include?('event')
+        events_urls << url if url.include?('event') && url.exclude?('tickets')
       end
       browser.close
     end
@@ -58,18 +58,21 @@ class Scraper
       url = "https://ra.co/#{events_url}"
       html = (open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, 'User-Agent' => 'opera'))
       doc = Nokogiri::HTML(html)
-      title = doc.css('.Text-sc-1t0gn2o-0.lYqGv').text.gsub("\n", '').gsub("\r", '')
-      title2 = doc.css('.Text-sc-1t0gn2o-0.llxwqv').text.gsub("\n", '').gsub("\r", '')
-      location = doc.css('.Text-sc-1t0gn2o-0.Link__StyledLink-k7o46r-0.hvqKqA').first || 'Unknown location'
-      ad = doc.css('.Grid__GridStyled-sc-1l00ugd-0.hTDtOT.grid').css('.Text-sc-1t0gn2o-0.dhoduX')
-      start_date = doc.css('.Text-sc-1t0gn2o-0 Link__StyledLink-k7o46r-0.hpyRNz').last.text.gsub("\n", '').gsub("\r", '')
-      start_h = doc.css('.Text-sc-1t0gn2o-0.dhoduX').slice(1).text
-      end_h = doc.css('.Text-sc-1t0gn2o-0.dhoduX').slice(3).text
-      line_up = doc.css('.Text-sc-1t0gn2o-0.CmsContent__StyledText-g7gf78-0').text.gsub("\n", '').gsub("\r", '') # warning
-      prom = doc.css('.Text-sc-1t0gn2o-0.dhoduX').slice(4).text.gsub("\n", '').gsub("\r", '')
-      age = doc.css('.Box-omzyfs-0.kXpspU>span').text.include?('ageD')
-      age ? 'You better take 15 bucks, just in case' : price = doc.css('.Text-sc-1t0gn2o-0.dhoduX').last.text # warning
-      des = doc.css('.Text-sc-1t0gn2o-0.EventDescription__BreakText-a2vzlh-0.hPALEa').text.gsub("\n", '').gsub("\r", '')
+      title = doc.css('.Text-sc-1t0gn2o-0.llxwqv').text.gsub("\n", '').gsub("\r", '')
+      title2 = doc.css('.Text-sc-1t0gn2o-0.lYqGv').text.gsub("\n", '').gsub("\r", '')
+      location_data = doc.css('.Text-sc-1t0gn2o-0.Link__StyledLink-k7o46r-0.hpyRNz').first.text
+      location2 = doc.css('.Text-sc-1t0gn2o-0.cvGKBC').text
+      reg_digit = /\d/
+      location_data.match(reg_digit) ? location = location2 : location = location_data
+      ad = doc.css('.Grid__GridStyled-sc-1l00ugd-0.itbbwg.grid').css('.Text-sc-1t0gn2o-0.dhoduX').text
+      start_date = doc.css('.Text-sc-1t0gn2o-0.Link__StyledLink-k7o46r-0.hpyRNz')[1] || doc.css('.Text-sc-1t0gn2o-0.Link__StyledLink-k7o46r-0.hpyRNz').first
+      # start_h = doc.css('.Text-sc-1t0gn2o-0.dhoduX').slice(1).text
+      # end_h = doc.css('.Text-sc-1t0gn2o-0.dhoduX').slice(3).text
+      line_up = doc.css('[data-tracking-id=event-detail-lineup]').text.gsub('Lineup', '')
+      # prom = doc.css('.Text-sc-1t0gn2o-0.dhoduX').slice(4).text.gsub("\n", '').gsub("\r", '')
+      # age = doc.css('.Box-omzyfs-0.kXpspU>span').text.include?('ageD')
+      # age ? 'You better take 15 bucks, just in case' : price = doc.css('.Text-sc-1t0gn2o-0.dhoduX').last.text # warning
+      des = doc.css('.Text-sc-1t0gn2o-0.EventDescription__BreakText-a2vzlh-0.kcYnvu').text.gsub("\n", '').gsub("\r", '')
       img_urls = []
       img_links = doc.css('.FullWidthStyle-sc-4b98ap-0.htnFjY>img')
       img_links.each do |img_link|
@@ -80,9 +83,9 @@ class Scraper
       reg_h = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/
       event_info = {
         title: title.empty? ? title2 : title,
-        location: location.text,
-        address: ad.empty? || ad.nil? ? 'Hmmm good luck, you should guess' : ad.first.text.gsub("\r", '').gsub("\n", ''),
-        start_time: start_date,
+        location: location || 'Unknown location',
+        address: ad.empty? || ad.nil? ? 'Hmmm good luck, you should guess' : ad,
+        start_time: start_date.text,
         # start_h: start_h.match(reg_h)[0],
         # end_h: end_h.match(reg_h)[0],
         line_up: line_up,
@@ -93,8 +96,7 @@ class Scraper
         # price: price.nil? ? 'Take 15 just in case' : price
       }
       events << event_info
-      p events
     end
-    return events
+     events
   end
 end
